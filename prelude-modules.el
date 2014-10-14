e880ae97a2e00b432542d404fac38c51066aef1d
Colin Williams <lackita@gmail.com>
2014-10-14 11:11:49 -0400

    initial customizations

---
+++ prelude-modules.el
@@ -0,0 +1,33 @@
+;;; Uncomment the modules you'd like to use and restart Prelude afterwards
+
+;; Emacs IRC client
+(require 'prelude-erc)
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-helm)
+(require 'prelude-helm-everywhere)
+(require 'prelude-company)
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
+(require 'prelude-mediawiki)
+
+(require 'prelude-c)
+(require 'prelude-clojure)
+(require 'prelude-coffee)
+(require 'prelude-common-lisp)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+(require 'prelude-erc)
+(require 'prelude-erlang)
+(require 'prelude-haskell)
+(require 'prelude-js)
+(require 'prelude-latex)
+(require 'prelude-lisp)
+(require 'prelude-mediawiki)
+(require 'prelude-org)
+(require 'prelude-perl)
+(require 'prelude-python)
+(require 'prelude-ruby)
+(require 'prelude-scala)
+(require 'prelude-scheme)
+(require 'prelude-scss)
+(require 'prelude-web)
+(require 'prelude-xml)
