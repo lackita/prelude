9b130a206b0aecf14ee7c2fdfb7b9aa4273e1030
Colin Williams <lackita@gmail.com>
2015-09-21 12:40:57 -0400

    add mysql option

--- personal/custom.el
+++ personal/custom.el
@@ -20,7 +20,10 @@
  '(cperl-use-syntax-table-text-property-for-tags t)
  '(indent-tabs-mode t)
  '(kill-whole-line t)
- '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96)))
+ '(sql-mysql-options (quote ("-A")))
+ '(tab-stop-list
+   (quote
+	(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96)))
  '(tab-width 4)
  '(web-mode-tag-auto-close-style 1))
 (custom-set-faces
